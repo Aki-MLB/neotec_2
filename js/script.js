e4accb2a754e4bcff9f95213e13d5e5c63fb7c4b
aki-MLB <akibgloo.ch@gmail.com>
2022-02-24 03:01:50 +0900

    first

---
+++ js/script.js
@@ -0,0 +1,100 @@
+$(function(){
+	
+	var
+	  winW = $(window).width(),
+		winH = $(window).height(),
+		nav = $('#mainnav ul a'),
+		curPos = $(this).scrollTop();
+	
+	if (winW < 880){
+		var headerH =0;
+	}
+	else{
+		var headerH =63;
+	}
+	
+	$(nav).on('click', function(){
+		nav.removeClass('active');
+  	var $el = $(this),
+		id = $el.attr('href');
+ 		$('html, body').animate({
+   		scrollTop: $(id).offset().top - headerH
+ 		}, 500);
+		$(this).addClass('active');
+		if (winW < 880){
+			$('#menuWrap').next().slideToggle();
+			$('#menuBtn').removeClass('close');
+		}
+ 		return false;
+	});
+	
+	var timer = false;
+	$(window).bind('load resize',function(){	
+		if (timer !== false){clearTimeout(timer);}
+		timer = setTimeout(function(){
+			var
+				w = $(window).innerWidth(),
+				bg = $('.bg'),
+				bgH = bg.height();
+			
+			if(w > 800){
+				$(function(){		
+			  	$(".vMid").css('height', bgH);
+				});
+			}
+			else{
+				$(function(){		
+			  	$(".vMid").css({'height':'auto','padding':'50px 20px'});
+				});
+			}		
+		});
+	});
+	
+	$('.panel').hide();
+	$('#menuWrap').toggle(function(){
+		$(this).next().slideToggle();
+		$('#menuBtn').toggleClass('close');
+	},
+	function(){
+		$(this).next().slideToggle();
+		$('#menuBtn').removeClass('close');
+	});
+		
+	$(window).on('scroll', function(){
+		var curPos = $(this).scrollTop();
+		if(curPos > 80){
+			$('#mainnav').addClass('changeNav');
+		}
+		//else{
+			//$('#mainnav').removeClass('changeNav');
+		//}
+	});
+
+
+});
+//ドロップダウンの設定を関数でまとめる
+function mediaQueriesWin(){
+	var width = $(window).width();
+	if(width <= 768) {//横幅が768px以下の場合 $(".has-child>a").off('click');	//has-childクラスがついたaタグのonイベントを複数登録を避ける為offにして一旦初期状態へ
+		$(".has-child>a").on('click', function() {//has-childクラスがついたaタグをクリックしたら
+			var parentElem =  $(this).parent();// aタグから見た親要素のliを取得し
+			$(parentElem).toggleClass('active');//矢印方向を変えるためのクラス名を付与して
+			$(parentElem).children('ul').stop().slideToggle(500);//liの子要素のスライドを開閉させる※数字が大きくなるほどゆっくり開く
+			return false;//リンクの無効化
+		});
+	}else{//横幅が768px以上の場合
+		$(".has-child>a").off('click');//has-childクラスがついたaタグのonイベントをoff(無効)にし
+		$(".has-child>a").removeClass('active');//activeクラスを削除
+		$('.has-child').children('ul').css("display","");//スライドトグルで動作したdisplayも無効化にする
+	}
+}
+
+// ページがリサイズされたら動かしたい場合の記述
+$(window).resize(function() {
+	mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load',function(){
+	mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
+});
